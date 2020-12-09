8aa8c7cc2984477679e7b1a7d0aff17d61590fc1
danyluis <danyluis@gmail.com>
2020-12-09 18:57:37 +0100

    AoC 2020: Day 8 - Merge both parts

--- 2020/08-handheld/08.py
+++ 2020/08-handheld/08.py
@@ -43,10 +43,38 @@ def run_and_break(program, context):
         op(value, context)
         times_run[position] += 1
 
+def does_it_finish(program):
+    context = Context()
+    times_run = defaultdict(int)
+    while 0 <= context.pc < len(program):
+        position, op, value = program[context.pc]
+        if times_run[position] == 1:
+            return (False, context.register)
+        op(value, context)
+        times_run[position] += 1
+    return (True, context.register)
+
+
 def main():
     context = Context()
     program = load_program()
     run_and_break(program, context)
     print(f'Accumulator value before any instruction is executed a second time is: {context.register}')
+
+    changes = {
+        jmp: nop,
+        nop: jmp
+    }
+
+    for position, op, value in (line for line in program if line.op in (jmp, nop)):
+        program[position] = operation(position, changes[op], value)
+        finished, register = does_it_finish(program)
+
+        if finished:
+            print(f'After changing {op.__name__} in position {position} to {changes[op].__name__}, the program finishes with Accumulator=={register}')
+            break
+
+        program[position] = operation(position, op, value)
+
 
 main()
