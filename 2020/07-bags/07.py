19622fd94aab0016520d31dcb938e1f5697da95b
danyluis <danyluis@gmail.com>
2020-12-09 18:49:55 +0100

    AoC 2020: Merge Day 7's part I & II

--- 2020/07-bags/07.py
+++ 2020/07-bags/07.py
@@ -11,8 +11,7 @@ from collections import namedtuple
 bag = namedtuple("bag", ("color", "number"))
 
 color_description_re = re.compile('(\\d+) (\\w+ \\w+) bags?')
-non_empty_bag_re = re.compile("^((\\w+ ){2})bags contain(.+)\\.")
-empty_bag_re = re.compile('^((\\w+ ){2})bags contain no other bags\\.')
+non_empty_bag_re = re.compile("^(\\w+ \\w+) bags contain (\\d.+)\\.")
 
 
 def load_graph():
@@ -20,18 +19,13 @@ def load_graph():
 
     for line in sys.stdin:
         line = line.strip()
-        if empty_bag_re.match(line):
-            color, foo = empty_bag_re.findall(line)[0]
-        elif non_empty_bag_re.match(line):
-            color, foo, included = non_empty_bag_re.findall(line)[0]
+        if non_empty_bag_re.match(line):
+            color, included = non_empty_bag_re.findall(line)[0]
             color = color.strip()
 
             for part in included.split(','):
                 number, included_color = color_description_re.findall(part)[0]
-
                 number = int(number)
-                included_color = included_color.strip()
-
                 graph[color].add(bag(included_color, number))
     return graph
 
@@ -46,7 +40,21 @@ def can_get_from_to(start, end, graph):
     return False
 
 
+def count_bags(color, graph):
+    cnt = 1
+
+    if not graph[color]:
+        return cnt
+
+    for bag_color, number in graph[color]:
+        cnt += number * count_bags(bag_color, graph)
+
+    return cnt
+
+
 graph = load_graph()
 containing_shiny_gold = sum([1 for color in set(graph.keys()) if can_get_from_to(color, 'shiny gold', graph)])
 print(f'Bag colors with shiny gold inside: {containing_shiny_gold}')
 
+contained = count_bags('shiny gold', graph) - 1
+print(f'1 shiny gold bag contains {contained} bags')
